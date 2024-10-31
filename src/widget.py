ca1f4c4aad76916d4c20db562b9e9c3e0b020a4f
Mikhail Kuznetsov <k9140707193@ya.ru>
2024-10-31 09:27:35 +0700

    сократил код, вместо перебора for работаем через .split

--- src/widget.py
+++ src/widget.py
@@ -3,15 +3,10 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(type_and_number: str) -> str:
     """ Функция, которая маскирует номер счета или карты"""
-    text_result = ""
-    digit_result = ""
-    digit_count = 0
-    for el in type_and_number:
-        if el.isalpha() or el.isspace():
-            text_result += el
-        elif el.isdigit():
-            digit_result += el
-            digit_count += 1
+    parts = type_and_number.split() # разбиваем строку на части
+    text_result = " ".join(parts[:-1]) # определяем тип карты или счет
+    digit_result = parts[-1] # получаем номер карты/счета
+    digit_count = len(digit_result)
     if digit_count > 16:
         return f"{text_result.strip()} {get_mask_account(digit_result)}"
     else:
@@ -20,7 +15,7 @@ def mask_account_card(type_and_number: str) -> str:
 
 print(mask_account_card("Visa Platinum 7000792289606361"))
 
-
+'''
 def get_date(data_time: str) -> str:
     """Функция, которая изменяет формат даты"""
     data_time_new = data_time[:10]
@@ -30,4 +25,4 @@ def get_date(data_time: str) -> str:
 
 data_input = get_date("2024-03-11T02:26:18.671407")
 print(data_input)
-
+'''
