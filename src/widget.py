78c207089a42862d8937c64b1a09e290a9df0c6f
Mikhail Kuznetsov <k9140707193@ya.ru>
2024-10-31 09:33:35 +0700

    отменил комментирование блока

--- src/widget.py
+++ src/widget.py
@@ -15,7 +15,7 @@ def mask_account_card(type_and_number: str) -> str:
 
 print(mask_account_card("Visa Platinum 7000792289606361"))
 
-'''
+
 def get_date(data_time: str) -> str:
     """Функция, которая изменяет формат даты"""
     data_time_new = data_time[:10]
@@ -25,4 +25,4 @@ def get_date(data_time: str) -> str:
 
 data_input = get_date("2024-03-11T02:26:18.671407")
 print(data_input)
-'''
+
