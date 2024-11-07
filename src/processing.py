1d06229bcaf9099c46ba6bb76ac4a24bbf79a7b8
Mikhail Kuznetsov <k9140707193@ya.ru>
2024-11-07 12:15:51 +0700

    add black, fixes processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,15 @@
-def filter_by_state(list_dict: list[dict], state='EXECUTED')-> list[dict]:
+def filter_by_state(list_dict: list[dict], state="EXECUTED") -> list[dict]:
     """Функция, формирующая данные по указанному параметру"""
-    return [el for el in list_dict if el['state'] == state]
+    return [el for el in list_dict if el["state"] == state]
 
 
-status_klient = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+status_klient = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 
-state = 'CANCELED'
+state = "CANCELED"
 print(filter_by_state(status_klient, state))
-
