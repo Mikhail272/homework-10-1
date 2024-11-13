294dbb6f4f521684ced692f2ba59f825571b55ff
Mikhail Kuznetsov <k9140707193@ya.ru>
2024-11-13 13:48:32 +0700

    закомментировал два принта в модуле widget

--- src/widget.py
+++ src/widget.py
@@ -13,7 +13,7 @@ def mask_account_card(type_and_number: str) -> str:
         return f"{text_result.strip()} {get_mask_card_number(digit_result)}"
 
 
-print(mask_account_card("Visa Platinum 7000792289606361"))
+# print(mask_account_card("Visa Platinum 7000792289606361"))
 
 
 def get_date(data_time: str) -> str:
@@ -23,5 +23,5 @@ def get_date(data_time: str) -> str:
     return f"{day}.{month}.{year}"
 
 
-data_input = get_date("2024-03-11T02:26:18.671407")
-print(data_input)
+# data_input = get_date("2024-03-11T02:26:18.671407")
+# print(data_input)
