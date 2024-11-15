fd2fb0a3446ec75e1d409511904d96e4522d08b1
Mikhail Kuznetsov <k9140707193@ya.ru>
2024-11-15 17:05:49 +0700

    доработаны модули test_widget, masks

---
+++ tests/test_widget.py
@@ -0,0 +1,46 @@
+import pytest
+from src.widget import mask_account_card, get_mask_account, get_mask_card_number
+
+@pytest.fixture
+def expected_masked_cards():
+    return [
+        ("Visa 7000792289606361", "Visa 7000 79** **** 6361"),  # Корректная карта
+        ("MasterCard 1234567812345678", "MasterCard 1234 56** **** 5678"),  # Корректная карта
+        ("Bank Account 1234567890123456789", "Bank Account **6789"),       # Корректный счет
+    ]
+
+@pytest.mark.parametrize("input_data,expected_output", [
+    ("Visa 7000792289606361", "Visa 7000 79** **** 6361"),
+    ("MasterCard 1234567812345678", "MasterCard 1234 56** **** 5678"),
+    ("Bank Account 1234567890123456789", "Bank Account **6789"),
+    ("Savings Account 11112222333344445555", "Savings Account **5555"),
+])
+def test_mask_account_card(input_data, expected_output):
+    assert mask_account_card(input_data) == expected_output
+
+
+@pytest.mark.parametrize("invalid_input", [
+    "InvalidInput",  # Некорректный ввод
+    "",              # Пустая строка
+    "Visa",          # Нет номера
+    "MasterCard 123", # Неверный формат номера
+])
+def test_mask_account_card_invalid(invalid_input):
+    with pytest.raises(ValueError):  # Возникает ошибка в случае некорректного ввода
+        mask_account_card(invalid_input)
+
+
+@pytest.mark.parametrize("account_number,expected_output", [
+    ("73654108430135874305", "**4305"),
+    ("788", "Неверный формат номера счета"),
+])
+def test_get_mask_account(account_number, expected_output):
+    assert get_mask_account(account_number) == expected_output
+
+
+@pytest.mark.parametrize("card_number,expected_output", [
+    ("7000792289606361", "7000 79** **** 6361"),
+    ("800079228960636", "Неверный формат банковской карты"),
+])
+def test_get_mask_card_number(card_number, expected_output):
+    assert get_mask_card_number(card_number) == expected_output
